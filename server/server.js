560f40eae881836f4d00558c7c38482dfb8be396
makejun168 <873800030@qq.com>
2020-04-07 18:20:26 +0800

    update

--- server/server.js
+++ server/server.js
@@ -1,11 +1,25 @@
 const express = require('express')
 const fs = require('fs')
 const path = require('path')
+const bodyParser = require('body-parser')
+const session = require('express-session')
 const favicon = require('serve-favicon')
 const ReactSSR = require('react-dom/server')
 const isDev = process.env.NODE_ENV === 'development' // 判断是否是开发环境 或者 生产环境
 
 const app = express()
+
+// 转化为 json 数据
+app.use(bodyParser.json())
+// 对应 post 请求的 formData 统一转化为 req.body
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(session({
+	maxAge: 10 * 60 * 1000,
+	name: 'tid',
+	resave: false,
+	saveUninitialized: false,
+	secret: 'kobe-bryant'
+}))
 
 app.use(favicon(path.join(__dirname, '../favicon.ico')))
 
